97281be9bcf0d0033c5ecb1eac68948ab658d430
Kudusch <kudusch@localhost.localdomain>
2021-04-07 15:47:21 +0200

    Fix path handling

--- render.sh
+++ render.sh
@@ -4,12 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 curl -L https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data > $DIR/meta_data/RKI_COVID19.csv
 
-python3 get_data.py '14713'
-python3 get_data.py '05515'
-python3 get_data.py '05913'
-python3 get_data.py '03403'
-python3 get_data.py '05334'
-python3 get_data.py '05978'
+python3 $DIR/get_data.py '14713'
+python3 $DIR/get_data.py '05515'
+python3 $DIR/get_data.py '05913'
+python3 $DIR/get_data.py '03403'
+python3 $DIR/get_data.py '05334'
+python3 $DIR/get_data.py '05978'
 
 
 Rscript $DIR/render.R
