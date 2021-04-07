fb7177c7c8ab4e1cc54876071406647ccc6288ae
Kudusch <kudusch@localhost.localdomain>
2021-04-07 12:51:56 +0200

    Fix path handling and adjust plot legend

--- render.sh
+++ render.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -L https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data > meta_data/RKI_COVID19.csv
-python3 get_data.py '14713'
-python3 get_data.py '05515'
-python3 get_data.py '05913'
+curl -L https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data > /var/www/html/projects/covid-ms/meta_data/RKI_COVID19.csv
+python3 /var/www/html/projects/covid-ms/get_data.py '14713'
+python3 /var/www/html/projects/covid-ms/get_data.py '05515'
+python3 /var/www/html/projects/covid-ms/get_data.py '05913'
 
-Rscript render.R
+Rscript /var/www/html/projects/covid-ms/render.R
