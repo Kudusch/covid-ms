1e052de472051b9e931f803fbbb1e465cdf87628
Tim Schatto-Eckrodt <kudusch@posteo.de>
2021-04-27 12:55:02 +0200

    Add information on Notbremse

--- render.sh
+++ render.sh
@@ -6,6 +6,8 @@ mkdir -p $DIR/meta_data
 
 curl -L https://www.arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data > $DIR/meta_data/RKI_COVID19.csv
 
+curl https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Daten/Fallzahlen_Kum_Tab.xlsx?__blob=publicationFile > $DIR/meta_data/Fallzahlen_Kum_Tab.xlsx
+
 python3 $DIR/get_data.py '14713'
 python3 $DIR/get_data.py '05515'
 python3 $DIR/get_data.py '05913'
