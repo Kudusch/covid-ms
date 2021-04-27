856671700ea1649d219d29537775213af325fa96
Tim Schatto-Eckrodt <kudusch@posteo.de>
2021-04-27 15:33:30 +0200

    Add information on corrected incidence

--- render.sh
+++ render.sh
@@ -16,4 +16,27 @@ python3 $DIR/get_data.py '05334'
 python3 $DIR/get_data.py '05978'
 python3 $DIR/get_data.py '05513'
 
+#curl https://www.corona-kvwl.de/praxisinformationen/corona-schutzimpfung/impfberichte | grep -io 'fileadmin.*\.pdf' > files.txt 
+
+CUR_DATE=$(date +%d_%m_%Y)
+
+curl https://www.corona-kvwl.de/fileadmin/user_upload/pdf/praxisinfos/corona_impfungen/COVID-19_Impfbericht_Praxen_$CUR_DATE.pdf > $DIR/meta_data/Praxen.pdf
+
+curl https://www.corona-kvwl.de/fileadmin/user_upload/pdf/praxisinfos/corona_impfungen/COVID-19_Impfbericht_Impfzentren_$CUR_DATE.pdf > $DIR/meta_data/Impfzentren.pdf
+
+curl https://www.corona-kvwl.de/fileadmin/user_upload/pdf/praxisinfos/corona_impfungen/COVID-19_Impfbericht_mobile_Teams_$CUR_DATE.pdf > $DIR/meta_data/Teams.pdf
+
+convert -density 150 -trim $DIR/meta_data/Praxen.pdf $DIR/meta_data/page%d.png
+tesseract -l deu $DIR/meta_data/page2.png $DIR/meta_data/Praxen
+
+convert -density 150 -trim $DIR/meta_data/Impfzentren.pdf $DIR/meta_data/page%d.png
+tesseract -l deu $DIR/meta_data/page2.png $DIR/meta_data/Impfzentren
+
+convert -density 150 -trim $DIR/meta_data/Teams.pdf $DIR/meta_data/page%d.png
+tesseract -l deu $DIR/meta_data/page0.png $DIR/meta_data/Teams
+
+grep -i "Münster" $DIR/meta_data/*.txt > $DIR/meta_data/vacc_numbers.csv
+
+rm $DIR/meta_data/*.pdf $DIR/meta_data/*.png $DIR/meta_data/*.txt
+
 Rscript $DIR/render.R
